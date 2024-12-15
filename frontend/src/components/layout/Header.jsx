475e17a7bca067470127f789f1e54f8dc8915e36
Donya <thisis2nya@gmail.com>
2024-12-15 14:06:47 +0330

    feat(header): add logo & title which is a link to home page

--- frontend/src/components/layout/Header.jsx
+++ frontend/src/components/layout/Header.jsx
@@ -1,5 +1,8 @@
 "use client";
 
+import Link from "next/link";
+import Image from "next/image";
+
 import NavLink from "./NavLink";
 // import { useAuth } from "@/context/AuthContext";
 
@@ -27,8 +30,13 @@ function Header() {
       className={`z-10 shadow-md bg-inherit mb-10 sticky top-0 transition-all duration-200 border-b border-b-secondary-300 ${
         isLoading ? "blur-sm opacity-70" : "opacity-100 blur-0"
       }`}>
-      <nav className="container xl:max-w-screen-xl">
-        <ul className="flex items-center text-secondary-400 justify-between py-2">
+      <nav className="container xl:max-w-screen-xl flex items-center gap-20">
+        <Link href="/" className="flex items-center justify-start gap-3">
+          <Image src="/logo.png" width={28} height={200} />
+          <h1 className="text-xl text-zinc-200 font-bold">بلاگینو</h1>
+        </Link>
+
+        <ul className="w-full flex items-center text-secondary-400 justify-between py-2">
           <div className="flex items-center gap-x-10">
             {navLinks.map((navLink) => {
               return (
